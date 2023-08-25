7d2be42185d25ce1387452dcf0541e2784022197
Memo Chou <memochou1993@gmail.com>
2023-08-26 00:20:55 +0800

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { reply } from '../services/line.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post('/webhook', async (req, res) => {
+  const events = req.body.events || [];
+  const replies = events
+    .filter(({ type }) => type === 'message')
+    .map(({ replyToken, message }) => reply({
+      replyToken,
+      messages: [{ type: 'text', text: message.text }],
+    }));
+  await Promise.all(replies);
+  res.sendStatus(200);
+});
+
+app.listen(3000);
+
+export default app;
---
+++ services/line.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const instance = axios.create({
+  baseURL: 'https://api.line.me',
+  timeout: 9000,
+  headers: {
+    Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+  },
+});
+
+/**
+ * @param {string} replyToken
+ * @param {Array<Object>} messages
+ * @param {string} messages[].type
+ * @param {string} messages[].text
+ */
+const reply = ({
+  replyToken,
+  messages,
+}) => instance.post('/v2/bot/message/reply', {
+  replyToken,
+  messages,
+});
+
+export {
+  reply,
+};
+
+export default null;
